a5bf4f7127600d82f2bd36e61a2c11f20a676364
Dani Mayfield <dani@neon.cc>
2023-02-27 18:40:12 -0500

    chore: remove unnecessary components & update head

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Dani Mayfield</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Dani Mayfield front end developer portfolio"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
