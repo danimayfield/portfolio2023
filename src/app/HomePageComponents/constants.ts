987b2d68e1286a9b31a8b42f861f081b8340e483
Dani Mayfield <dani@neon.cc>
2023-03-01 12:29:33 -0500

    chore: add projects data in constants

---
+++ src/app/HomePageComponents/constants.ts
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    name: '1AND1',
+    skills: ['react native', 'typescript', 'redux', 'redux toolkit'],
+  },
+  {
+    name: 'Animal Health Link',
+    skills: ['react', 'typescript', 'graphQL', 'apollo', 'directus', 'docker'],
+  },
+  {
+    name: 'Fast Forward',
+    skills: ['react', 'typescript', 'graphQL', 'apollo', 'directus', 'mapbox'],
+  },
+];
