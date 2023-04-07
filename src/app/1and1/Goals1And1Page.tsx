6e1bf6819005da0a65bb474a0521ec14d7b19b54
Dani Mayfield <dani@neon.cc>
2023-04-07 11:59:41 -0400

    style: add hero & title to 1and1 page

--- src/app/1and1/Goals1And1Page.tsx
+++ src/app/1and1/Goals1And1Page.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Center, Text, useTheme } from '@chakra-ui/react';
+import { HeroColors } from '@shared/components/styled';
 
 export const Goals1And1Page = () => {
+  const { colors } = useTheme();
   return (
-    <div>Goals1And1Page</div>
-  )
-}
+    <HeroColors color1={colors.sky[400]} color3={colors.gray[200]}>
+      <Center>
+        <Text zIndex={400} fontSize="3xl">
+          1AND1
+        </Text>
+      </Center>
+    </HeroColors>
+  );
+};
