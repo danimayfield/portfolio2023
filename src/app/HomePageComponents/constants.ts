fcf732386dec4238b9c9753305a0044ad898b4c8
Dani Mayfield <dani@neon.cc>
2023-04-07 12:24:23 -0400

    chore: update project data constants

--- src/app/HomePageComponents/constants.ts
+++ src/app/HomePageComponents/constants.ts
@@ -7,29 +7,70 @@ export type Project = {
   name: string;
   id: string;
   skills: string[];
-  description: string;
-  favourite: string;
   imageSrc: string;
   alt: string;
   route: string;
 };
 
+export type ProjectData = {
+  name: string;
+  skills: string[];
+  description: string;
+  favourite: string;
+};
+
+export enum Projects {
+  G1AND1 = '1AND1',
+  AHL = 'AHL',
+  FF = 'FF',
+}
+
 export const projects: Project[] = [
   {
     name: '1AND1',
-    id: '1AND1',
+    id: Projects.G1AND1,
     skills: ['react native', 'typescript', 'redux', 'redux toolkit'],
-    description:
-      "I was apart of the end to end creation and deployment of the 1AND1 personal wellness app. As a V1 I implemented the redux state management to manage the data which is stored on the phone's local storage. As well as I developed all the features within the application in typescript react native using NativeBase's UI Framework.",
-    favourite:
-      'My favourite part about working on this project was implementing a performance enhancing event queue that saves user events to a queue and sends them periodically to machine learning to track how real users use the app to influence new features.',
     imageSrc: Feature1and1.src,
     alt: 'Mobile mockup collage of 1and1 app',
     route: routes.goals1And1,
   },
   {
     name: 'Animal Health Link',
-    id: 'AHL',
+    id: Projects.AHL,
+    skills: [
+      'react',
+      'typescript',
+      'graphQL',
+      'apollo',
+      'directus',
+      'docker',
+      'dialpad integration',
+    ],
+    imageSrc: FeatureAHL.src,
+    alt: 'Tablet mockup Animal Health Link application',
+    route: routes.animalHealthLink,
+  },
+  {
+    name: 'Fast Forward',
+    id: Projects.FF,
+    skills: ['react', 'typescript', 'graphQL', 'apollo', 'mapbox'],
+    imageSrc: FeatureFastForward.src,
+    alt: 'Desktop mockup of Fast Forward Application',
+    route: routes.fastForward,
+  },
+];
+
+export const projectData: Record<Projects, ProjectData> = {
+  [Projects.G1AND1]: {
+    name: '1AND1',
+    skills: ['react native', 'typescript', 'redux', 'redux toolkit'],
+    description:
+      "I was apart of the end to end creation and deployment of the 1AND1 personal wellness app. As a V1 I implemented the redux state management to manage the data which is stored on the phone's local storage. As well as I developed all the features within the application in typescript react native using NativeBase's UI Framework.",
+    favourite:
+      'My favourite part about working on this project was implementing a performance enhancing event queue that saves user events to a queue and sends them periodically to machine learning to track how real users use the app to influence new features.',
+  },
+  [Projects.AHL]: {
+    name: 'Animal Health Link',
     skills: [
       'react',
       'typescript',
@@ -43,20 +84,13 @@ export const projects: Project[] = [
       'I was apart of the end to end development of this custom triage web application. I created features using react typescript & Chakra UI and working with a GraphQL/Apollo backend which used directus to help manage. The main feature of this app is a form flow that call center users must be able to navigate to any point in the flow easily. This app also features two-way communication via SMS enabling users to recieve texts & images from customers.',
     favourite:
       'My favourite part about working on this project was implementing different customizations to make the use and flow of the application further tailored to the specific RVT agents that will be using it. Customizations include implementing a dark mode for users on the night shift as well as generating pdfs of submitted incidents that are easily shareable.',
-    imageSrc: FeatureAHL.src,
-    alt: 'Tablet mockup Animal Health Link application',
-    route: routes.animalHealthLink,
   },
-  {
+  [Projects.FF]: {
     name: 'Fast Forward',
-    id: 'FF',
     skills: ['react', 'typescript', 'graphQL', 'apollo', 'mapbox'],
     description:
       "I was apart of the creation and development of Fast Forward's custom map application. This application uses Mapbox to display and manage Fast Forward's assets for the regions they service. I developed and customized the points on the map, adding ability to filter them based on different properties and developing search functionalty amoung other main features. This was all done within typescript react using Chakra UI and a graphQL & apollo backend.",
     favourite:
       'My favourite part about working on this project was getting to fully integrate the Mapbox API within a web application. As well as creating an admin area that allows for certain users to add new assets with different image types in bulk, set new map boundaries & publish map tiles that will display within the main application.',
-    imageSrc: FeatureFastForward.src,
-    alt: 'Desktop mockup of Fast Forward Application',
-    route: routes.fastForward,
   },
-];
+};
