eb53e5af316d28b31dedb8c7055d9a083042a76c
Dani Mayfield <danimmayfield@gmail.com>
2024-12-22 14:04:43 -0500

    chore: change margin to padding

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -14,8 +14,8 @@ export const HomePage = () => {
   return (
     <Box as="main" bg="white">
       <HomeHeroSection />
-      <HomeContentSection mt="8%" mb="20%" />
-      <HomeAboutSection mb="20%" id="about" />
+      <HomeContentSection mt="8%" />
+      <HomeAboutSection mb="20%" pt="20%" id="about" />
       <HomeWorksSection id="works" />
       <ContactSection id="contact" />
     </Box>
